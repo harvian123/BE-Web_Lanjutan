5652b539836078a24d17a80739a372da93e53cea
shuraa75 <harviann123@gmail.com>
2021-05-17 08:32:23 +0700

    no message

--- index.js
+++ index.js
@@ -25,14 +25,14 @@ conn.connect(function(err){
 })
 app.get('/', function(req,res){
     res.send(`
-        <form method="post" action="/todo">
-            <input name="nama" style="margin-right:10px"/>
-            <input type="submit"/>
+        <form method="post" action="/todo" style="margin-right:10px">
+            <input name="myInput"/>
+            <input type="submit" value="tambahkan"/>
         </form>
     `)
 })
 app.post('/todo',function(req,res){
-    const sql = `INSERT INTO items (nama) VALUES (\'${req.body.nama}\')`
+    const sql = `INSERT INTO items (nama) VALUES (\'${req.body.myInput}\')`
     conn.query(sql,function(err){
         if(err) 
             throw err
@@ -52,7 +52,7 @@ app.get('/todo',function(req,res){
 })
 
 app.delete('/todo/:nama',function(req,res){
-    const query = `DELETE FROM items where nama=\'${req.params.nama}\'`
+    const query = `DELETE FROM items where nama=\'${req.params.myInput}\'`
     conn.query(query,function(err,result){
         if(err)
             throw err
